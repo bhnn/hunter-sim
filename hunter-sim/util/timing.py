ad299192982f418530539020049b93093ff97098
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-01-07 19:24:12 +0100

    Added code timing util

---
+++ hunter-sim/util/timing.py
@@ -0,0 +1,29 @@
+# https://stackoverflow.com/a/1557906/4443082
+import atexit
+from functools import reduce
+from time import perf_counter as clock
+
+
+def secondsToStr(t):
+    return "%d:%02d:%02d.%03d" % reduce(lambda ll,b : divmod(ll[0],b) + ll[1:], [(t*1000,),1000,60,60])
+
+line = "="*40
+def log(s, elapsed=None):
+    print(line)
+    print(secondsToStr(clock()), '-', s)
+    if elapsed:
+        print("Elapsed time:", elapsed)
+    print(line)
+    print
+
+def endlog():
+    end = clock()
+    elapsed = end-start
+    log("End Program", secondsToStr(elapsed))
+
+def now():
+    return secondsToStr(clock())
+
+start = clock()
+atexit.register(endlog)
+log("Start Program")
