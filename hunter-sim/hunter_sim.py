ad1890e638151b1973a4cf303a5bb7600cc887d0
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-02-21 17:46:37 +0100

    Moved config validation from sim.py to hunter_sim.py

--- hunter-sim/hunter_sim.py
+++ hunter-sim/hunter_sim.py
@@ -11,13 +11,14 @@ from sim import SimulationManager
 from util.exceptions import BuildConfigError
 
 
-def main(path: str, compare_path: str, num_sims: int, dump_config: str, processes: int, verbose: bool, log: bool):
+def main(path: str, compare_path: str, num_sims: int, show_stats: bool, dump_config: str, processes: int, verbose: bool, log: bool):
     """Main entry point for the simulation tool.
 
     Args:
         path (str): Path to a valid build config file
         compare_path (str): Optional: path to a second valid build config file to compare against the first
         num_sims (int): Number of simulations to run
+        show_stats (bool): Whether to show combat statistics after the simulation, only the stage breakdown and loot
         dump_config (str): Optional: whether to dump empty build config files to ./builds/ directory and exit
         threads (int): Number of threads to use for parallelisation. -1 for sequential processing.
         verbose (bool): Whether to print verbose output to stdout
@@ -41,15 +42,22 @@ def main(path: str, compare_path: str, num_sims: int, dump_config: str, processe
         sys.exit(0)
     if not path and not dump_config:
         print("hunter_sim.py: error: the following arguments are required: -f/--path")
+        sys.exit(1)
     if num_sims == 1 and verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     try:
-        if path and compare_path:
-            with open(path, 'r') as f:
-                hunter1 = yaml.safe_load(f)["meta"]["hunter"]
+        with open(path, 'r') as f:
+            cfg1 = yaml.safe_load(f)
+        if cfg1["meta"]["hunter"].lower() not in ["borge", "ozzy"]:
+            print("hunter_sim.py: error: invalid hunter found in primary build config file. Please specify a valid hunter.")
+            sys.exit(1)
+        if compare_path:
             with open(compare_path, 'r') as f:
-                hunter2 = yaml.safe_load(f)["meta"]["hunter"]
-            if hunter1 != hunter2:
+                cfg2 = yaml.safe_load(f)
+            if cfg2["meta"]["hunter"].lower() not in ["borge", "ozzy"]:
+                print("hunter_sim.py: error: invalid hunter found in secondary build config file. Please specify a valid hunter.")
+                sys.exit(1)
+            if cfg1["meta"]["hunter"] != cfg2["meta"]["hunter"]:
                 print("hunter_sim.py: error: cannot compare builds of different hunters")
                 sys.exit(1)
         if num_sims == 1 and log:
@@ -61,13 +69,13 @@ def main(path: str, compare_path: str, num_sims: int, dump_config: str, processe
                 force=True,
                 level=logging.DEBUG,
             )
-        smgr = SimulationManager(path)
+        smgr = SimulationManager(cfg1)
         if compare_path:
             import timing
-            smgr.compare_against(compare_path, num_sims, num_processes=processes)
+            smgr.compare_against(cfg2, num_sims, num_processes=processes, show_stats=show_stats)
         else:
             import timing
-            smgr.run(num_sims, num_processes=processes)
+            smgr.run(num_sims, num_processes=processes, show_stats=show_stats)
     except FileNotFoundError:
         print("hunter_sim.py: error: build config file not found. Please specify the correct name or run with the -d flag to generate an empty build config file.")
         sys.exit(1)
@@ -82,6 +90,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--path", type=str, help="Path to a valid build config .yaml file")
     parser.add_argument("-c", "--compare_builds", type=str, help="Path to a second valid build config .yaml file to compare against the first", dest="compare_path")
     parser.add_argument("-i", "--num-sims", type=int, help="Number of simulations to run", default=100)
+    parser.add_argument("-s", "--no-stats", action="store_false", help="Suppresses display of combat statistics after the simulation, only the stage breakdown and loot.", dest="show_stats")
     parser.add_argument("-d", "--dump-config", action="store_true", help="Save an empty config file to ./builds/ directory and exits.")
     parser.add_argument("-t", "--processes", type=int, help="Number of processes to use for parallelisation. -1 for sequential processing.", default=-1, dest="processes")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print simulation progress to stdout")
