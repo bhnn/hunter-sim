1c28fd2913c25cae599f24d83af8fc07b2a67504
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-02-23 11:17:39 +0100

    Added tables to present final stage milestones and stage result breakdown

--- hunter-sim/sim.py
+++ hunter-sim/sim.py
@@ -91,7 +91,15 @@ class SimulationManager():
         return res
 
     @classmethod
-    def prepare_results(cls, res_dict: Dict) -> Dict:
+    def prepare_results(cls, res_dict: Dict) -> Tuple[Dict, Dict]:
+        """Turn a simulation's result dictionary into averages and standard deviations.
+
+        Args:
+            res_dict (Dict): Results of simulations.
+
+        Returns:
+            Tuple[Dict, Dict]: Averages and standard deviations of the results.
+        """
         res_avg, res_std = {}, {}
         hunter = res_dict.pop('hunter')
         # Enrages
@@ -132,7 +140,7 @@ class SimulationManager():
             'sustain': ['damage_taken', 'regenerated_hp', 'attacks_suffered', 'lifesteal'],
             'defence': ['evades', 'trickster_evades', 'mitigated_damage'],
             'effects': ['effect_procs', 'stun_duration_inflicted', 'helltouch_barrier', 'life_of_the_hunt_healing', 'echo_bullets', 'unfair_advantage_healing'],
-            'loot*': ['loot_per_hour'],
+            'loot': ['loot_per_hour'],
         }
         for k, v in output_format.items():
             res_avg[k] = {val: avg[val] for val in v if val in avg}
@@ -142,41 +150,68 @@ class SimulationManager():
         res_std['main']['elapsed_time'] = timedelta(seconds=round(std["elapsed_time"]))
         res_avg['effects']['stun_duration_inflicted'] = timedelta(seconds=round(avg['stun_duration_inflicted']))
         res_std['effects']['stun_duration_inflicted'] = timedelta(seconds=round(std['stun_duration_inflicted']))
-        res_avg['loot*'].update({'best_lph': max(res_dict['loot_per_hour']), 'worst_lph': min(res_dict['loot_per_hour'])})
-        res_std['loot*'].update({'best_lph': 0, 'worst_lph': 0})
-        res_avg['final_stage'] = {'max': max(res_dict['final_stage']), 'med': floor(statistics.median(res_dict['final_stage'])), 'avg': floor(statistics.mean(res_dict['final_stage'])), 'min': min(res_dict['final_stage'])}
+        res_avg['loot'].update({'best_lph': max(res_dict['loot_per_hour']), 'worst_lph': min(res_dict['loot_per_hour'])})
+        res_std['loot'].update({'best_lph': 0, 'worst_lph': 0})
+        res_avg['final_stage'] = {
+            'aggregates': {'highest': max(res_dict['final_stage']), 'median': floor(statistics.median(res_dict['final_stage'])), 'average': floor(statistics.mean(res_dict['final_stage'])), 'lowest': min(res_dict['final_stage'])},
+            'chances': {i:j/len(res_dict["final_stage"]) for i,j in dict(sorted(Counter(res_dict["final_stage"]).items())).items()},
+            }
         res_avg['is_comparison'] = False
         return res_avg, res_std
 
     @classmethod
     def make_comparable(cls, dict1: Dict, dict2: Dict) -> Tuple[Dict, Dict]:
-        res, diff = {}, {}
-        res['is_comparison'] = not dict1.pop('is_comparison')
+        """Make two result dictionaries comparable for display.
+
+        Args:
+            dict1 (Dict): Average stats dictionary for build 1.
+            dict2 (Dict): Average stats dictionary for build 2.
+
+        Returns:
+            Tuple[Dict, Dict]: Flat and percentage difference dictionaries for the two builds.
+        """
+        flat_diff, pct_diff = {}, {}
+        flat_diff['is_comparison'] = not dict1.pop('is_comparison')
         for k, v in dict1.items():
             if k not in ['final_stage']:
-                res[k], diff[k] = {}, {}
+                flat_diff[k], pct_diff[k] = {}, {}
                 for vk in v:
                     if vk not in ['elapsed_time', 'worst_lph', 'enrage_stacks:_1st_boss', 'enrage_stacks:_2nd_boss', 'enrage_stacks:_3rd_boss']:
                         if dict1[k][vk] > dict2[k][vk]:
-                            res[k][vk] = dict1[k][vk] - dict2[k][vk]
-                            diff[k][vk] = (dict1[k][vk] / dict2[k][vk]) - 1 if dict2[k][vk] != 0 else float('inf') # BUILD 1
+                            flat_diff[k][vk] = dict1[k][vk] - dict2[k][vk]
+                            pct_diff[k][vk] = (dict1[k][vk] / dict2[k][vk]) - 1 if dict2[k][vk] != 0 else float('inf') # BUILD 1
                         else:
-                            res[k][vk] = dict2[k][vk] - dict1[k][vk]
+                            flat_diff[k][vk] = dict2[k][vk] - dict1[k][vk]
                             # negative value signals output function that this is build2 instead of 1
-                            diff[k][vk] = -1 * ((dict2[k][vk] / dict1[k][vk]) - 1) if dict1[k][vk] != 0 else -1 # BUILD 2
+                            pct_diff[k][vk] = -1 * ((dict2[k][vk] / dict1[k][vk]) - 1) if dict1[k][vk] != 0 else -1 # BUILD 2
                     else:
                         if dict2[k][vk] > dict1[k][vk]:
-                            res[k][vk] = dict2[k][vk] - dict1[k][vk]
-                            diff[k][vk] = (dict2[k][vk] / dict1[k][vk]) - 1 if dict1[k][vk] != 0 else -1 # BUILD 1
+                            flat_diff[k][vk] = dict2[k][vk] - dict1[k][vk]
+                            pct_diff[k][vk] = (dict2[k][vk] / dict1[k][vk]) - 1 if dict1[k][vk] != 0 else -1 # BUILD 1
                         else:
-                            res[k][vk] = dict1[k][vk] - dict2[k][vk]
-                            diff[k][vk] = -1 * ((dict1[k][vk] / dict2[k][vk]) - 1) if dict2[k][vk] != 0 else float('inf') # BUILD 2
-        res.update({'final_stage': {'build_1': dict1['final_stage'], 'build_2': dict2['final_stage']}})
-        return res, diff
+                            flat_diff[k][vk] = dict1[k][vk] - dict2[k][vk]
+                            pct_diff[k][vk] = -1 * ((dict1[k][vk] / dict2[k][vk]) - 1) if dict2[k][vk] != 0 else float('inf') # BUILD 2
+        flat_diff.update({'final_stage': {'build_1': dict1['final_stage'], 'build_2': dict2['final_stage']}})
+        return flat_diff, pct_diff
 
     @classmethod
     def display_stats(cls, dict1: Dict, dict2: Dict, show_stats: bool) -> None:
+        """Display combat statistics in a table.
+
+        Args:
+            dict1 (Dict): Average stats dictionary for single runs, or flat difference dictionary for comparisons.
+            dict2 (Dict): Standard deviation dictionary for single runs, or percentage difference dictionary for comparisons.
+            show_stats (bool): Whether to show combat statistics after the simulation, or only the stage breakdown and loot.
+        """
         def get_all_values(d: Dict) -> Generator:
+            """Nested helper function to yield all values from a nested dictionary.
+
+            Args:
+                d (Dict): Dictionary to extract values from.
+
+            Yields:
+                Generator: Values from the dictionary.
+            """
             for _, v in d.items():
                 if not isinstance(v, dict):
                     if not isinstance(v, timedelta):
@@ -186,82 +221,104 @@ class SimulationManager():
 
         console = rich.get_console()
         # base table
-        table = rich.table.Table(title="Combat Statistics", show_header=True, header_style="bold dim cyan", caption='*) Loot values are arbitrary and for build comparison only.')
-        table.add_column("Category", style="dim cyan")
-        table.add_column("Statistic", style="cyan")
+        stats_table = rich.table.Table(title="Combat Statistics", expand=True, show_header=True, header_style="bold dim cyan", caption='*) Loot values are arbitrary and for build comparison only.\n\u2020) Smaller values are better here.')
         if is_comparison := dict1.pop('is_comparison'):
-            # add diff, qualifier and winner columns for comparisons, max_width calc without decimals (2nd dict is just "BUILD 1/2")
-            table.add_column("Average diff.", justify='right', style="yellow")
-            table.add_column("Qual.", justify='left', style="dim yellow")
-            table.add_column(r"% diff", justify='right', style="magenta")
-            table.add_column("Winner", justify='center', style="magenta")
+            # different from regular runs because comparisons use row highlights for builds1/2, so no colours needed
+            stats_table.add_column("Category", style="dim cyan")
+            stats_table.add_column("Statistic",)
+            stats_table.add_column("Average diff.", justify='right',)
+            stats_table.add_column(r"% diff", justify='right',)
+            stats_table.add_column("Winner", justify='center',)
+            # some options: light_goldenrod1, light_goldenrod2, light_pink1, light_salmon1, light_salmon3, pale_violet_red1,
+            # misty_rose3, tan, dark_sea_green1, dark_sea_green2, light_steel_blue, pale_green1, 
+            cb1 = '[light_salmon1]'
+            cb2 = '[light_steel_blue]'
+            cbn = '[dim]'
         else:
-            # add average and stdev columns for regular runs, max_width calc with decimals
-            table.add_column("Average", justify='right', style="yellow")
-            table.add_column("+/- Std Dev", justify='right', style="dim yellow")
-        keys_to_display = ['main', 'offence', 'sustain', 'defence', 'effects', 'loot*'] if show_stats else ['loot*']
+            stats_table.add_column("Category", style="dim cyan")
+            stats_table.add_column("Statistic", style="cyan")
+            stats_table.add_column("Average", justify='right', style="yellow")
+            stats_table.add_column("+/- Std Dev", justify='right', style="dim yellow")
+        keys_to_display = ['main', 'offence', 'sustain', 'defence', 'effects', 'loot'] if show_stats else ['loot']
+        # for combat stat table column widths, adjusted via output formatting
         max_width_avg = max([max(len(f'{k:,.2f}') for k in get_all_values(dict1))])
         max_width_std = max([max(len(f'{k:,.2f}') for k in get_all_values(dict2))])
         for k in keys_to_display:
             last_key = ''
             for subkey in dict1[k]:
                 main_cat = capwords(k if k != last_key else '')
+                if main_cat == 'Loot': main_cat += '*'
                 if subkey in ['best_lph', 'worst_lph']:
+                    # to capitalise LPH, capwords doesn't do that
                     cstat = ' '.join([capwords(subkey.split('_')[0]), 'LPH'])
                 else:
                     cstat = ' '.join(capwords(subkey).split('_'))
+                # no decimal formatting for timedeltas
                 val = f'{str(dict1[k][subkey]):>{max_width_avg}}' if isinstance(dict1[k][subkey], timedelta) else f'{dict1[k][subkey]:>{max_width_avg},.2f}'
                 if is_comparison:
-                    if subkey in ['worst_lph']:
-                        qual = 'less'
-                    elif subkey in ['elapsed_time']:
-                        qual = 'faster'
-                    elif subkey in ['enrage_stacks:_1st_boss', 'enrage_stacks:_2nd_boss', 'enrage_stacks:_3rd_boss']:
-                        qual = 'fewer'
-                    elif subkey in ['first_revive_stage', 'second_revive_stage']:
-                        qual = 'later'
-                    else:
-                        qual = 'more'
+                    row_style = cb1 if dict2[k][subkey] > 0 else cb2 if dict2[k][subkey] < 0 else cbn
+                    cstat = f'{row_style}{cstat}'
+                    val = f'{row_style}{val}'
+                    if subkey in ['elapsed_time', 'worst_lph', 'enrage_stacks:_1st_boss', 'enrage_stacks:_2nd_boss', 'enrage_stacks:_3rd_boss']:
+                        cstat += '[dim]\u2020'
                     if dict2[k][subkey] > 0:
-                        diff = f'{abs(dict2[k][subkey]):>{max_width_std},.2%}'
-                        winner = '1'
+                        diff = f'{row_style}{abs(dict2[k][subkey]):>{max_width_std},.2%}'
+                        winner = f'{row_style}1'
                     elif dict2[k][subkey] < 0:
-                        diff = f'{abs(dict2[k][subkey]):>{max_width_std},.2%}'
-                        winner = '2'
+                        diff = f'{row_style}{abs(dict2[k][subkey]):>{max_width_std},.2%}'
+                        winner = f'{row_style}2'
                     else:
-                        diff = '-'
-                        winner = '-'
-                    table.add_row(main_cat, cstat, val, qual, diff, winner)
+                        diff = f'{row_style}-'
+                        winner = f'{row_style}-'
+                    stats_table.add_row(main_cat, cstat, val, diff, winner)
                 else:
                     stdev = f'{str(dict2[k][subkey]):>{max_width_std}}' if isinstance(dict1[k][subkey], timedelta) else f'{dict2[k][subkey]:>{max_width_std},.2f}'
-                    table.add_row(main_cat, cstat, val, stdev)
+                    stats_table.add_row(main_cat, cstat, val, stdev)
                 if last_key != k:
                     # so the main category is only shown everytime it changes, makes for a less busy table
                     last_key = k
-            table.add_section()
-        console.print(table)
-        # TODO: add final stage to table as new section, not looking good like this
-        # grid = rich.table.Table(title="Final stage", show_header=True, box=None, header_style="bold dim cyan" )
-        # grid.add_column("Final stage", justify='center', style="dim cyan")
-        # grid.add_column("Max", justify='right', style="cyan")
-        # grid.add_column("Med", justify='right', style="cyan")
-        # grid.add_column("Avg", justify='right', style="cyan")
-        # grid.add_column("Min", justify='right', style="cyan")
-        # grid.add_row(
-        #     'Build 1',
-        #     str(dict1['final_stage']['build_1']['max']),
-        #     str(dict1['final_stage']['build_1']['med']),
-        #     str(dict1['final_stage']['build_1']['avg']),
-        #     str(dict1['final_stage']['build_1']['min'])
-        # )
-        # grid.add_row(
-        #     'Build 2',
-        #     str(dict1['final_stage']['build_2']['max']),
-        #     str(dict1['final_stage']['build_2']['med']),
-        #     str(dict1['final_stage']['build_2']['avg']),
-        #     str(dict1['final_stage']['build_2']['min'])
-        # )
-        # console.print(grid)
+            stats_table.add_section()
+        stage_table = rich.table.Table(title="Final Stage Highlights", show_header=True, header_style="bold dim cyan", expand=True)
+        stage_table.add_column("Build", style="cyan")
+        stage_table.add_column("Highest", justify='right', style="cyan")
+        stage_table.add_column("Median", justify='right', style="cyan")
+        stage_table.add_column("Average", justify='right', style="cyan")
+        stage_table.add_column("Lowest", justify='right', style="cyan")
+        if is_comparison:
+            # add both builds + min-level alerts in case of boss deaths
+            row1 = [f'Build 1', *map(str, dict1['final_stage']['build_1']['aggregates'].values())]
+            row2 = [f'Build 2', *map(str, dict1['final_stage']['build_2']['aggregates'].values())]
+            if dict1['final_stage']['build_1']['aggregates']['highest'] > dict1['final_stage']['build_1']['aggregates']['lowest'] and dict1['final_stage']['build_1']['aggregates']['lowest'] % 100 == 0:
+                row1[-1] = f"[red]{row1[-1]} ({dict1['final_stage']['build_1']['chances'][dict1['final_stage']['build_1']['aggregates']['lowest']]:.2%})"
+            if dict1['final_stage']['build_2']['aggregates']['highest'] > dict1['final_stage']['build_2']['aggregates']['lowest'] and dict1['final_stage']['build_2']['aggregates']['lowest'] % 100 == 0:
+                row2[-1] = f"[red]{row2[-1]} ({dict1['final_stage']['build_2']['chances'][dict1['final_stage']['build_2']['aggregates']['lowest']]:.2%})"
+            stage_table.add_row(*row1, style=cb1[1:-1])
+            stage_table.add_row(*row2, style=cb2[1:-1])
+        else:
+            # just add build 1 for single-hunter simulations
+            row1 = ['Build 1', *map(str, dict1['final_stage']['aggregates'].values())]
+            if dict1['final_stage']['aggregates']['highest'] > dict1['final_stage']['aggregates']['lowest'] and dict1['final_stage']['aggregates']['lowest'] % 100 == 0:
+                row1[-1] = f"[red]{row1[-1]} ({dict1['final_stage']['chances'][dict1['final_stage']['aggregates']['lowest']]:.2%})"
+            stage_table.add_row(*row1)
+        if not is_comparison:
+            # add stage breakdown panel for single-hunter simulations
+            stage_breakdown_table = rich.table.Table(title="Stage Results").grid(padding=0, expand=True)
+            items_per_row = 6
+            for _ in range(items_per_row):
+                stage_breakdown_table.add_column("", style="cyan")
+                stage_breakdown_table.add_column("", style="dim")
+            # dict1['final_stage']['chances'] = dict(sorted(dict1['final_stage']['chances'].items(), key=lambda item: item[0]))
+            stages = list(map(lambda x: f'{x:>4}', dict1['final_stage']['chances'].keys()))
+            chances = list(map(lambda x: f'{x:>7,.2%}', dict1['final_stage']['chances'].values()))
+            for i in range(0, len(dict1['final_stage']['chances'].keys()), items_per_row):
+                row = [e for e in chain(*zip(stages[i:i+items_per_row], chances[i:i+items_per_row]))]
+                stage_breakdown_table.add_row(*row)
+            sbt_panel = rich.panel.Panel(stage_breakdown_table, title="Stage Result Breakdown", border_style="bold dim cyan")
+        table_group = rich.console.Group(
+            *[stats_table, stage_table, sbt_panel] if not is_comparison else [stats_table, stage_table],
+        )
+        panel = rich.panel.Panel.fit(table_group, title="Simulation Results", border_style="bold dim cyan")
+        console.print(panel)
 
 
 class Simulation():
